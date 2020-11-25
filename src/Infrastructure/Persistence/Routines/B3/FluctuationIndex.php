4a560db7a589f3b1e258a075cc8afe2537cefcbf
Francisco Jr <francisco.antonio@ascbrazil.com.br>
2020-11-25 20:21:25 -0300

    Atualização do indice ibov/ifix

--- src/Infrastructure/Persistence/Routines/B3/FluctuationIndex.php
+++ src/Infrastructure/Persistence/Routines/B3/FluctuationIndex.php
@@ -6,66 +6,37 @@ namespace App\Infrastructure\Persistence\Routines\B3;
 use App\Domain\Index\Index;
 use App\Domain\IndexComposition\IndexComposition;
 use App\Domain\Ticker\Ticker;
+use GuzzleHttp\Client;
+
+$client = new Client();
 
 $list_index = Index::all()->toArray();
 
 foreach ($list_index as $index) {
-    $list_composition = IndexComposition::where('index_id', $index['id'])->get();
+    $response = $client->request('GET', 'http://cotacao.b3.com.br/mds/api/v1/DailyFluctuationHistory/'.$index['name']);
+    
+    $data = $response->getBody()->getContents();
+    if (!empty($data)) {
+        $data = json_decode($data, true);
 
-    $index = array_merge($index, [
-        'open' => '0.00',
-        'high' => '0.00',
-        'close' => '0.00',
-        'low' => '0.00',
-        'change' => '0.00',
-        'change_abs' => '0.00',
-    ]);
+        $history = $data['TradgFlr']['scty']['lstQtn'];
+        $last_history = end($history);
+        
+        $history_price = [];
+        array_walk($history, function($v, $k) use (&$history_price) {
+            $history_price[] = $v['closPric'];
+        });
 
-    foreach ($list_composition as $composition) {
-        $ticker = Ticker::where('name', $composition['name'])->first()->toArray();
-
-        $index['open'] = bcadd(
-            $index['open'],
-            bcmul(
-                number_format((float) $ticker['open'], 2),
-                number_format((float) $composition['theoretical_qtd'], 2),
-                2
-            ),
-            2
-        );
-
-        $index['high'] = bcadd(
-            $index['high'],
-            bcmul(
-                number_format((float) $ticker['high'], 2),
-                number_format((float) $composition['theoretical_qtd'], 2),
-                2
-            ),
-            2
-        );
-
-        $index['close'] = bcadd(
-            $index['close'],
-            bcmul(
-                number_format((float) $ticker['close'], 2),
-                number_format((float) $composition['theoretical_qtd'], 2),
-                2
-            ),
-            2
-        );
-
-        $index['low'] = bcadd(
-            $index['low'],
-            bcmul(
-                number_format((float) $ticker['low'], 2),
-                number_format((float) $composition['theoretical_qtd'], 2),
-                2
-            ),
-            2
-        );
-
-        $index['change_abs'] = bcsub($index['close'], $index['open'], 2);
-        $index['change'] = bcmul(bcdiv($index['change_abs'], $index['close'], 4), '100', 2);
+        $index = array_merge($index, [
+            'open' => $history[0]['closPric'],
+            'high' => max($history_price),
+            'close' => $last_history['closPric'],
+            'low' => min($history_price),
+            'change' => round($last_history['prcFlcn'], 2),
+            'change_abs' => bcsub((string) $last_history['closPric'], (string) $history[0]['closPric'], 2),
+        ]);
+        unset($history);
+        unset($history_price);
     }
     
     $index = array_filter($index, function($v, $k) {
